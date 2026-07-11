f04ac81a880fd18902a2fb589d304c42e2ac26af
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2026-07-11 18:06:53 +0900

    Install GNOME Desktop and Japanese language pack
    Added installation commands for GNOME Desktop and Japanese language pack.

--- server-GnomeDesktop.sh
+++ server-GnomeDesktop.sh
@@ -13,3 +13,12 @@ sudo timedatectl set-timezone Asia/Tokyo
 # システム更新
 echo "===== apt update && upgrade ====="
 sudo apt update && sudo apt upgrade -y
+
+# GNOMEデスクトップ環境と日本語化パック
+echo "===== Install GNOME Desktop And Japanese Language Pack ====="
+sudo apt install gnome-session gdm3 gnome-terminal fonts-noto-cjk language-pack-gnome-ja -y
+# gnome-session（GNOMEデスクトップ環境）
+# gdm3（GNOMEディスプレイマネージャ。要はログイン画面）
+# gnome-terminal：GNOME標準ターミナル（他のターミナルでもOK。gnome-session はターミナルをインストールしない。）
+# fonts-noto-cjk：日本語フォント（これだけでターミナル内は日本語表示できる）
+# language-pack-gnome-ja（GNOMEデスクトップ向け日本語翻訳パック。language-pack-ja も依存でインストールされる）
